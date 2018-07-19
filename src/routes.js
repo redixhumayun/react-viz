c254552f4efbb8c6328d9af1ec47d134bd42d11c
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro.local>
2018-07-19 23:20:31 +0530

    updated sql query

--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,22 @@ router
     ctx.set('Access-Control-Allow-Origin', '*')
     const { fromDate, toDate } = ctx.params
     try {
-      await sql.connect(`mssql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTIP}/PPMDAT`)
-      let result = await sql.query`SELECT LOCATION, PRDDATE, CAST(SUM(SAMPRD) as DECIMAL(16, 0)) AS TTLSAMS,
-                        SUM(NOFMAC) as TTLMAC FROM daily_prod(${fromDate}, ${toDate}) AS P GROUP BY LOCATION, PRDDATE`
+      const config = {
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        server: process.env.DB_HOSTIP,
+        database: 'PPMDAT'
+      }
+      const pool = await new sql.ConnectionPool(config).connect()
+      const result = await pool.request().query(`SELECT LOCATION,
+                    PRDDATE,
+                    TTLSAMS,
+                    TTLMAC,
+                    CAST((TTLSAMS/(TTLMAC*480)*100) as DECIMAL(16, 4)) AS SEW_EFF
+                    FROM (
+                      SELECT LOCATION, PRDDATE, CAST(SUM(SAMPRD) as DECIMAL(16, 0)) AS TTLSAMS,
+                      AVG(NOFMAC) as TTLMAC FROM daily_prod(${fromDate}, ${toDate}) GROUP BY LOCATION, PRDDATE
+                    ) AS P GROUP BY LOCATION, PRDDATE, TTLSAMS, TTLMAC`)
       sql.close()
       ctx.body = result
     } catch (error) {
