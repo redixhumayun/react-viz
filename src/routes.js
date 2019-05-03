afca0227f37d9b07602c85f208fae4744f42b6b4
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro.local>
2019-05-04 01:29:21 +0530

    All charts rendering and back button working

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,24 @@ const moment = require('moment')
 const formatString = (location) => location.trim()
 
 /**
+ * Converts date to moment object
+ * @param {string} date
+ * @returns {moment}
+ */
+const formatDate = (date) => {
+  const newDate = []
+  const currDate = date.toString()
+  for (let i = 0; i < currDate.length; i++) {
+    newDate.push(currDate[i])
+    if (i === 3 || i === 5) {
+      newDate.push('/')
+    }
+  }
+  // return moment(new Date(newDate.join(''))).format('MMMM DD, YYYY')
+  return moment(new Date(newDate.join('')))
+}
+
+/**
  * Function to clean up data by formatting date and trimming strings
  * @param {object} data
  * @return {object}
@@ -34,10 +52,10 @@ const cleanupData = (data) => {
 }
 
 /**
- * Function to group array of objects by key
+ * Function to group array of objects by key and then separate them into multiple arrays. Returns 2d array
  * @param {object} data
  * @param {string} key
- * @return {object}
+ * @return {array}
  */
 const groupBy = (data, key) => {
   return data.reduce((acc, curr) => {
@@ -48,30 +66,32 @@ const groupBy = (data, key) => {
 }
 
 /**
- * Converts date to moment object
- * @param {string} date
- * @returns {moment}
+ * Function to to convert the grouped object into a format compatible with D3
+ * @param {Object} data
+ * @param {String} key
  */
-const formatDate = (date) => {
-  const newDate = []
-  const currDate = date.toString()
-  for (let i = 0; i < currDate.length; i++) {
-    newDate.push(currDate[i])
-    if (i === 3 || i === 5) {
-      newDate.push('/')
-    }
-  }
-  return moment(new Date(newDate.join(''))).format('MMMM, DD')
+const convertToArray = (data, key) => {
+  return Object.entries(data).reduce((acc, [k, dataForDate]) => {
+    let reducedData = dataForDate.reduce((acc, dataForFactory) => {
+      let keyInObject = dataForFactory[key]
+      let eff = dataForFactory['EFF']
+      let date = dataForFactory['PRDDATE']
+      acc[keyInObject] = eff
+      acc['PRDDATE'] = date
+      return acc
+    }, {})
+    acc.push(reducedData)
+    return acc
+  }, [])
 }
 
 router
 /**
- * Route to get the overall factory data
+ * Data for a single day across factories
  */
-  .get('/:fromDate/:toDate', async (ctx, next) => {
+  .get('/:fromDate/:toDate', async (ctx) => {
     ctx.set('Access-Control-Allow-Origin', '*')
     const { fromDate, toDate } = ctx.params
-    console.log(fromDate, toDate)
     try {
       const config = {
         user: process.env.DB_USERNAME,
@@ -81,16 +101,17 @@ router
       }
       const pool = await new sql.ConnectionPool(config).connect()
       const result = await pool.request().query(`SELECT LOCATION,
-                    PRDDATE,
-                    TTLSAMS,
-                    TTLMAC,
-                    CAST((TTLSAMS/(TTLMAC*480)*100) as DECIMAL(16, 4)) AS SEW_EFF
-                    FROM (
-                      SELECT LOCATION, PRDDATE, CAST(SUM(SAMPRD) as DECIMAL(16, 0)) AS TTLSAMS,
-                      AVG(NOFMAC) as TTLMAC FROM daily_prod(${fromDate}, ${toDate}) GROUP BY LOCATION, PRDDATE
-                    ) AS P GROUP BY LOCATION, PRDDATE, TTLSAMS, TTLMAC`)
-      const formattedData = groupBy(cleanupData(result.recordset), 'LOCATION')
-      ctx.body = formattedData
+                                                  PRDDATE,
+                                                  (TTLSAMS/(NULLIF(TTLMAC * 480, 0))) * 100 AS EFF
+                                                  FROM(
+                                                    SELECT LOCATION, PRDDATE, SUM(NOFMAC) as TTLMAC, SUM(SAMPRD) as TTLSAMS
+                                                    FROM daily_prod(${fromDate}, ${toDate})
+                                                    GROUP BY LOCATION, PRDDATE
+                                                  ) AS P GROUP BY LOCATION, PRDDATE, TTLMAC, TTLSAMS`)
+      const formattedData = cleanupData(result.recordset)
+      const groupedData = groupBy(formattedData, 'PRDDATE')
+      const arrayData = convertToArray(groupedData, 'LOCATION')
+      ctx.body = arrayData
       await sql.close()
     } catch (error) {
       console.log(error)
@@ -98,10 +119,10 @@ router
     }
   })
 
-  /**
-   * Route to get the batch data for each factory
-   */
-  .get('/batchdata/:fromDate/:toDate', async (ctx, next) => {
+/**
+ * Route to get the overall factory data
+ */
+  .get('/averageEff/:fromDate/:toDate', async (ctx) => {
     ctx.set('Access-Control-Allow-Origin', '*')
     const { fromDate, toDate } = ctx.params
     try {
@@ -112,18 +133,47 @@ router
         database: 'PPMDAT'
       }
       const pool = await new sql.ConnectionPool(config).connect()
+      const result = await pool.request().query(`SELECT PRDDATE,
+                                                    (TTLSAMS/(NULLIF(TTLMAC * 480, 0))) * 100 AS EFF
+                                                    FROM(
+                                                      SELECT PRDDATE, SUM(NOFMAC) as TTLMAC, SUM(SAMPRD) as TTLSAMS
+                                                      FROM daily_prod(${fromDate}, ${toDate})
+                                                      GROUP BY PRDDATE
+                                                    ) AS P GROUP BY PRDDATE, TTLMAC, TTLSAMS`)
+      ctx.body = cleanupData(result.recordset)
+      await sql.close()
+    } catch (error) {
+      console.log(error)
+      sql.close()
+    }
+  })
+
+  /**
+   * Route to get the batch data for a factory on a single day
+   */
+  .get('/batchdata/:date/:location', async (ctx) => {
+    ctx.set('Access-Control-Allow-Origin', '*')
+    const { date, location } = ctx.params
+    try {
+      const config = {
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        server: process.env.DB_HOSTIP,
+        database: 'PPMDAT'
+      }
+      const pool = await new sql.ConnectionPool(config).connect()
       const result = await pool.request().query(
-        `SELECT LOCATION, BATCH, PRODUCT, PRDDATE, SAMS, PRODQTY, SAMPRD, NOFMAC
-        FROM daily_prod(${fromDate}, ${toDate})`
+        `SELECT LOCATION, BATCH, PRODUCT, PRDDATE,
+        (SAMPRD/NULLIF(NOFMAC * 480, 0)) * 100 as EFF
+        FROM (
+          SELECT LOCATION, BATCH, PRODUCT, PRDDATE, NOFMAC, SAMPRD, PRODQTY
+          FROM daily_prod(${date}, ${date}) WHERE LOCATION = '${location}'
+        ) AS P GROUP BY LOCATION, BATCH, PRODUCT, PRDDATE, NOFMAC, SAMPRD, PRODQTY`
       )
-      const cleanData = cleanupData(result.recordset)
-      const groupedData = groupBy(cleanData, 'PRDDATE')
-      const updatedData = Object.keys(groupedData).reduce((acc, curr) => {
-        const temp = groupBy(groupedData[curr], 'LOCATION')
-        acc[curr] = temp
-        return acc
-      }, {})
-      ctx.body = updatedData
+      const formattedData = cleanupData(result.recordset)
+      const groupedData = groupBy(formattedData, 'PRDDATE')
+      const arrayData = convertToArray(groupedData, 'BATCH')
+      ctx.body = arrayData
       await sql.close()
     } catch (error) {
       console.log(error)
